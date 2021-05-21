0753f2f6692bc8fdd5f5daa3e5c1456a3078ec98
javelin <tanakashoji@foxmail.com>
2021-05-21 20:17:42 +0800

    push second commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -3,7 +3,7 @@ package com.atguigu;
 public class git {
     public static void main(String[] args) {
         System.out.println("push");
-        System.out.println("heloo2");
+        System.out.println("push ssh");
         System.out.println("heloo git3");
         System.out.println("heloo hot-fix");
         System.out.println("text master");
