d2565300c73d0005c785120c03c1d87f5617cf7d
javelin <tanakashoji@foxmail.com>
2021-05-21 20:03:32 +0800

    master text commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("heloo2");
         System.out.println("heloo git3");
         System.out.println("heloo hot-fix");
+        System.out.println("text master");
     }
 }
