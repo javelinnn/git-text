360540de30441a46f92868941805e79e4a1b367c
javelin <tanakashoji@foxmail.com>
2021-05-21 20:15:22 +0800

    push commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -2,7 +2,7 @@ package com.atguigu;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("heloo");
+        System.out.println("push");
         System.out.println("heloo2");
         System.out.println("heloo git3");
         System.out.println("heloo hot-fix");
